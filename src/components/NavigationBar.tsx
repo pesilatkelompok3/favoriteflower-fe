19e935a5629b8ae304bae834583837d986df6986
vrlomhrn <vmahrian@gmail.com>
2023-08-11 21:10:21 +0700

    update color primary, secondary, accent

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,12 @@
+import { RxHamburgerMenu } from "react-icons/rx"
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between">
+      <h1>FavoriteFlower</h1>
+      <RxHamburgerMenu />
+    </nav>
+  )
+}
+
+export default Navbar
