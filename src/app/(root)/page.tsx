a561d4974f045241a2781e825bc0cddc2e63a341
thufeil23 <70207427+thufeil23@users.noreply.github.com>
2023-08-12 15:53:25 +0700

    add metadata

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,6 +2,13 @@ import About from "@/components/About";
 import ProductUs from "@/components/Home/productUs/ProductUs";
 import Testimonial from "@/components/Home/testimonial/Testimonial";
 import { Hero } from "@/components/Hero";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Beranda",
+  description: "Ini Halaman Beranda FavoriteFlowers",
+}
+
 
 export default function Home() {
   return (
