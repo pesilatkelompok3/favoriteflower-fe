eebc55887392c5b9921e353e67b2f3bb469c93a9
thufeil23 <70207427+thufeil23@users.noreply.github.com>
2023-08-12 15:20:09 +0700

    update footer

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -1,14 +1,12 @@
-
 'use client';
 
-import { RiInstagramLine, RiFacebookBoxLine, RiWhatsappLine } from "react-icons/ri";
 import {  BsFacebook, BsInstagram, BsWhatsapp } from 'react-icons/bs';
 import { Footer } from 'flowbite-react';
 
 export default function TheFooter() {
     return (
         <Footer container className="text-white bg-black">
-            <div className="w-full text-center lg:mx-96 lg:pt-12">
+            <div className="w-full text-center lg:mx-48 lg:pt-4 2xl:mx-64">
                 <div className="w-full justify-between sm:flex sm:items-start sm:justify-between gap-4">
                     <div>
                         <h1 className="text-2xl font-bold mb-4">
@@ -47,45 +45,3 @@ export default function TheFooter() {
         </Footer>
     )
 }
-
-
-
-// export function Footer() {
-//     return (
-//         <footer className="bg-slate-900 text-slate-50">
-//             <div>
-//                 <h1 className="text-md">
-//                     FavoriteFlower
-//                 </h1>
-//                 <p className="text-xs">
-//                     Lorem ipsum dolor sit amet consectetur. Quis amet ac est pellentesque pulvinar et ac sed lorem. Vitae viverra congue tellus ut. Ante nisl in sagittis vulputate. Molestie venenatis elit scelerisque aliquam egestas amet hac diam tincidunt lacus.
-//                 </p>
-//             </div>
-//             <div>
-//                 <h1 className="text-2xl">
-//                     Alamat
-//                 </h1>
-//                 <p className="text-sm">
-//                     Jl. Cihideung, Kab. Bandung,
-//                     Lembang, Jawa Barat
-//                 </p>
-//             </div>
-//             <div>
-//                 <h1 className="text-2xl">
-//                     Social Media
-//                 </h1>
-//                 <ul>
-//                     <li>
-//                         <RiFacebookLine />
-//                     </li>
-//                     <li>
-//                         <RiInstagramLine />
-//                     </li>
-//                     <li>
-//                         <RiWhatsappLine />
-//                     </li>
-//                 </ul>
-//             </div>
-//         </footer>
-//     )
-// }
