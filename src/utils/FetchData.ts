eceb69b6614be5fbe82af68a732e83f9aed3e7f1
vrlomhrn <vmahrian@gmail.com>
2023-08-12 18:29:00 +0700

    update utils fetchData

--- src/utils/FetchData.ts
+++ src/utils/FetchData.ts
@@ -1,13 +1,20 @@
 interface ResponseData {
   data: {};
 }
+
 type Props = {
   url: string;
 };
 
 const getData = async (url: Props["url"]): Promise<ResponseData> => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
   const data = await res.json();
+
   return data;
 };
 
