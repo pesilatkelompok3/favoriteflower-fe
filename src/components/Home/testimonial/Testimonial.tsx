0cebbbcc89477f3f17ae1bce93a8aee39e7359d3
CookyNdi <yandikun145@gmail.com>
2023-08-15 11:58:57 +0700

    adding testimonial section for home

--- src/components/Home/testimonial/Testimonial.tsx
+++ src/components/Home/testimonial/Testimonial.tsx
@@ -1,21 +1,49 @@
 import TestimonialCard from "./TestimonialCard";
 
 export default function Testimonial() {
+  const cardListStyle: React.CSSProperties = {
+    scrollbarWidth: "none", // For Firefox
+    msOverflowStyle: "none", // For Internet Explorer and Edge
+    WebkitOverflowScrolling: "touch", // For iOS Safari
+    scrollBehavior: "smooth",
+  };
+
+  const datas = [
+    {
+      id: "1",
+      name: "cokendi",
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eligendi, dicta possimus undedeserunt autem ipsa quam vel ratione nemo.",
+    },
+    {
+      id: "2",
+      name: "agato",
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eligendi, dicta possimus undedeserunt autem ipsa quam vel ratione nemo.",
+    },
+    {
+      id: "3",
+      name: "dachi",
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eligendi, dicta possimus undedeserunt autem ipsa quam vel ratione nemo.",
+    },
+  ];
+
   return (
-    <main className="w-full h-auto bg-slate-200 px-4 pt-3">
+    <div className="w-full h-auto bg-white px-4 md:px-16 pt-3">
       <div className="w-full h-auto flex justify-center items-center">
         <div className="w-24 md:w-96 bg-black" style={{ height: "1px" }}></div>
-        <h1 className="mx-4 text-lg font-bold underline">Testimonial</h1>
+        <h1 className="mx-4 text-lg font-bold underline">Testimoni</h1>
         <div className="w-24 md:w-96 bg-black" style={{ height: "1px" }}></div>
       </div>
-      <div className="w-full h-80 flex items-center">
-        <div className="w-full h-auto whitespace-nowrap scroll-auto overflow-hidden">
-          <TestimonialCard />
-          <TestimonialCard />
-          <TestimonialCard />
-          <TestimonialCard />
+      <div className="w-full h-[372px] relative md:px-28 flex items-center">
+        <div className="w-full h-auto md:p-4 whitespace-nowrap scroll-auto overflow-x-auto" style={cardListStyle}>
+          <style>{`::-webkit-scrollbar { display: none; }`}</style>
+          {datas.map((data) => (
+            <TestimonialCard key={data.id} dataKomen={data} />
+          ))}
         </div>
       </div>
-    </main>
+    </div>
   );
 }
--- src/components/Home/testimonial/TestimonialCard.tsx
+++ src/components/Home/testimonial/TestimonialCard.tsx
@@ -1,23 +1,31 @@
-import Image1 from "~/assets/images/7a41c59ebeff0bc91f253a0c85d18e01.jpeg";
+import Image1 from "~/assets/images/Chobi - すいちゃん今日も (94091894)  - Copy.png";
 import Image from "next/image";
 
-export default function TestimonialCard() {
-  const gradient =
-    "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 100%)";
+// Deklarasikan tipe props yang akan diteruskan ke dalam komponen
+type TestimonialCardProps = {
+  key: string; // React menggunakan key sebagai prop khusus, Anda mungkin tidak ingin meneruskannya
+  dataKomen: {
+    id: string;
+    name: string;
+    comment: string;
+    // ... tambahkan properti lain yang ada di objek dataKomen
+  };
+};
 
+const TestimonialCard: React.FC<TestimonialCardProps> = ({ dataKomen }) => {
   return (
-    <div className="w-40 h-40 relative inline-block justify-center rounded-3xl overflow-hidden ml-2 first:ml-0">
-      <Image
-        className="w-full h-full object-cover"
-        src={Image1}
-        alt="Picture of the author"
-      />
-      <div
-        className="w-full h-16 absolute bottom-0 pb-1 px-5 flex items-end"
-        style={{ background: gradient }}
-      >
-        <h1 className="text-base font-semibold mb-4">Flower Bouquet</h1>
+    <div className="w-72 h-80 md:w-80 md:h-[352px] relative rounded-3xl overflow-hidden inline-block drop-shadow-lg shadow-black/50 mr-2 md:mr-4">
+      <div className="w-full h-64 md:h-72 absolute bottom-0 pb-1 px-5 rounded-t-3xl bg-secondary flex flex-col items-center">
+        <div className="w-24 h-24 rounded-full overflow-hidden bg-black absolute -top-12">
+          <Image className="w-full h-full object-cover" src={Image1} alt="Picture of the author" />
+        </div>
+        <h1 className="mt-14 font-semibold text-lg text-center">{dataKomen.name}</h1>
+        <p className="mt-2 font-normal italic text-base text-center whitespace-normal">
+          &quot;{dataKomen.comment}&quot;
+        </p>
       </div>
     </div>
   );
-}
+};
+
+export default TestimonialCard;
