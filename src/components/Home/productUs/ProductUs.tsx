85e858ce7c548f11feac969dcfaf9a3ef64f46a8
thufeil23 <70207427+thufeil23@users.noreply.github.com>
2023-08-22 16:43:49 +0700

    Add Whatsapp Api

--- src/components/Home/productUs/ProductUs.tsx
+++ src/components/Home/productUs/ProductUs.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ProductCard from "@/components/Home/productUs/ProductCard";
-import { fetchData, formatPrice } from "@/lib/utils";
+import { fetchData } from "@/lib/utils";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
